977648619444bab68517aff19919ce964dc601bd
Ja'far Ramadan <23040271@student.ltuc.com>
2024-06-22 23:49:54 +0300

    Add project files.

---
+++ Contact-Manager-Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Contact_Manager_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Contact-Manager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Contact_Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
